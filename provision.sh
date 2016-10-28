5658df007aefbfff200866cdeeaaeacfb7648358
MarioG <mgonzale@dsic.upv.es>
2016-10-28 16:59:05 +0200

    Corregido error en provision.sh

--- provision.sh
+++ provision.sh
@@ -7,7 +7,7 @@ apt-get install -y nodejs
 npm install -g npm
 
 sudo apt-get -y install build-essential libtool autoconf automake uuid-dev
-sudo apt-get -y install libzmq-dev
+sudo apt-get -y install libzmq-dev libuv
 
 wget https://github.com/zeromq/zeromq4-1/releases/download/v4.1.5/zeromq-4.1.5.tar.gz
 tar xvzf zeromq-4.1.5.tar.gz
